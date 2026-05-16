54b8017d8642d4f68caa3c9855d3d175ca093817
Oliver Polydor <digitalisogni@gmail.com>
2026-05-16 12:58:11 +0200

    Fix blank page by restoring Vercel build sources and unregistering legacy service workers

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,19 @@ import { ThemeProvider } from './context/ThemeProvider.tsx';
 import { LocaleProvider } from './i18n/useLocale';
 import './index.css';
 
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then((registrations) => registrations.forEach((registration) => registration.unregister()))
+    .catch(() => null);
+  if ('caches' in window) {
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .catch(() => null);
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LocaleProvider>
